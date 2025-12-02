0a1cdcb5b13388000a5f3738a945aefb942bf231
Samman <asamman560@gmail.com>
2025-12-02 23:54:29 +0200

    Move Label Add New User Littel Up

--- frmAddNewUser.Designer.cs
+++ frmAddNewUser.Designer.cs
@@ -59,7 +59,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(329, 37);
+            this.label1.Location = new System.Drawing.Point(308, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(206, 32);
             this.label1.TabIndex = 0;
