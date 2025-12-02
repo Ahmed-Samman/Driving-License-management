512db0903455cb4c6bc4efba93aeafab4306d9d9
Samman <asamman560@gmail.com>
2025-12-02 23:32:20 +0200

    ADD Login Info UI In TabControl Tasks In frmAddNewUser.Designer.cs

--- frmAddNewUser.Designer.cs
+++ frmAddNewUser.Designer.cs
@@ -38,10 +38,20 @@
             this.tpage1PersonalInfo = new System.Windows.Forms.TabPage();
             this.ctrlPersonDetails1 = new Driving_License_management.ctrlPersonDetails();
             this.tpage2LoginInfo = new System.Windows.Forms.TabPage();
+            this.lalble1 = new System.Windows.Forms.Label();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.chkIsActive = new System.Windows.Forms.CheckBox();
             this.toolStripContainer1.ContentPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
             this.tabcPersonInfo.SuspendLayout();
             this.tpage1PersonalInfo.SuspendLayout();
+            this.tpage2LoginInfo.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -161,6 +171,15 @@
             // 
             // tpage2LoginInfo
             // 
+            this.tpage2LoginInfo.Controls.Add(this.chkIsActive);
+            this.tpage2LoginInfo.Controls.Add(this.txtConfirmPassword);
+            this.tpage2LoginInfo.Controls.Add(this.txtPassword);
+            this.tpage2LoginInfo.Controls.Add(this.txtUserName);
+            this.tpage2LoginInfo.Controls.Add(this.label4);
+            this.tpage2LoginInfo.Controls.Add(this.label3);
+            this.tpage2LoginInfo.Controls.Add(this.label2);
+            this.tpage2LoginInfo.Controls.Add(this.lblUserID);
+            this.tpage2LoginInfo.Controls.Add(this.lalble1);
             this.tpage2LoginInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tpage2LoginInfo.Location = new System.Drawing.Point(4, 29);
             this.tpage2LoginInfo.Name = "tpage2LoginInfo";
@@ -169,6 +188,88 @@
             this.tpage2LoginInfo.TabIndex = 1;
             this.tpage2LoginInfo.Text = "Login Info";
             this.tpage2LoginInfo.UseVisualStyleBackColor = true;
+            // 
+            // lalble1
+            // 
+            this.lalble1.AutoSize = true;
+            this.lalble1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lalble1.Location = new System.Drawing.Point(102, 64);
+            this.lalble1.Name = "lalble1";
+            this.lalble1.Size = new System.Drawing.Size(83, 22);
+            this.lalble1.TabIndex = 0;
+            this.lalble1.Text = "UserID: ";
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.Location = new System.Drawing.Point(235, 64);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(54, 22);
+            this.lblUserID.TabIndex = 1;
+            this.lblUserID.Text = "????";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UserName:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Confirm Password:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(239, 120);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(186, 26);
+            this.txtUserName.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(239, 182);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(186, 26);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(239, 241);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(186, 26);
+            this.txtConfirmPassword.TabIndex = 7;
+            // 
+            // chkIsActive
+            // 
+            this.chkIsActive.AutoSize = true;
+            this.chkIsActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkIsActive.Location = new System.Drawing.Point(239, 308);
+            this.chkIsActive.Name = "chkIsActive";
+            this.chkIsActive.Size = new System.Drawing.Size(104, 24);
+            this.chkIsActive.TabIndex = 8;
+            this.chkIsActive.Text = "Is Active";
+            this.chkIsActive.UseVisualStyleBackColor = true;
             // 
             // frmAddNewUser
             // 
@@ -184,6 +285,8 @@
             this.toolStripContainer1.PerformLayout();
             this.tabcPersonInfo.ResumeLayout(false);
             this.tpage1PersonalInfo.ResumeLayout(false);
+            this.tpage2LoginInfo.ResumeLayout(false);
+            this.tpage2LoginInfo.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -200,5 +303,14 @@
         private System.Windows.Forms.TabControl tabcPersonInfo;
         private System.Windows.Forms.TabPage tpage1PersonalInfo;
         private System.Windows.Forms.TabPage tpage2LoginInfo;
+        private System.Windows.Forms.Label lalble1;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkIsActive;
     }
 }
