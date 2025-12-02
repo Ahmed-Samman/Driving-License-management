57f25cea52670fbf5b81f08223c3600dd0907709
Samman <asamman560@gmail.com>
2025-12-02 12:15:52 +0200

    ADD frmFindPerson UI ( frmFindPerson.cs + frmFindPerson.designer.cs + frmFindPerson.resx )

---
+++ frmFindPerson.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Driving_License_management
+{
+    partial class frmFindPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrlPersonDetails1 = new Driving_License_management.ctrlPersonDetails();
+            this.label2 = new System.Windows.Forms.Label();
+            this.combFilterBy = new System.Windows.Forms.ComboBox();
+            this.txtIDorNationalNO = new System.Windows.Forms.TextBox();
+            this.btnAddNewPerson = new System.Windows.Forms.Button();
+            this.btnAddPerson = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(296, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Find person";
+            // 
+            // ctrlPersonDetails1
+            // 
+            this.ctrlPersonDetails1.Location = new System.Drawing.Point(3, 151);
+            this.ctrlPersonDetails1.Name = "ctrlPersonDetails1";
+            this.ctrlPersonDetails1.Size = new System.Drawing.Size(823, 276);
+            this.ctrlPersonDetails1.TabIndex = 2;
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Filter By:";
+            // 
+            // combFilterBy
+            // 
+            this.combFilterBy.FormattingEnabled = true;
+            this.combFilterBy.Location = new System.Drawing.Point(121, 95);
+            this.combFilterBy.Name = "combFilterBy";
+            this.combFilterBy.Size = new System.Drawing.Size(174, 28);
+            this.combFilterBy.TabIndex = 8;
+            // 
+            // txtIDorNationalNO
+            // 
+            this.txtIDorNationalNO.Location = new System.Drawing.Point(318, 98);
+            this.txtIDorNationalNO.Name = "txtIDorNationalNO";
+            this.txtIDorNationalNO.Size = new System.Drawing.Size(174, 26);
+            this.txtIDorNationalNO.TabIndex = 9;
+            // 
+            // btnAddNewPerson
+            // 
+            this.btnAddNewPerson.BackgroundImage = global::Driving_License_management.Properties.Resources.user__8_;
+            this.btnAddNewPerson.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAddNewPerson.Location = new System.Drawing.Point(572, 85);
+            this.btnAddNewPerson.Name = "btnAddNewPerson";
+            this.btnAddNewPerson.Size = new System.Drawing.Size(48, 47);
+            this.btnAddNewPerson.TabIndex = 11;
+            this.btnAddNewPerson.UseVisualStyleBackColor = true;
+            this.btnAddNewPerson.Click += new System.EventHandler(this.btnAddNewPerson_Click);
+            // 
+            // btnAddPerson
+            // 
+            this.btnAddPerson.BackgroundImage = global::Driving_License_management.Properties.Resources.salesman;
+            this.btnAddPerson.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAddPerson.Location = new System.Drawing.Point(508, 85);
+            this.btnAddPerson.Name = "btnAddPerson";
+            this.btnAddPerson.Size = new System.Drawing.Size(48, 47);
+            this.btnAddPerson.TabIndex = 10;
+            this.btnAddPerson.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = global::Driving_License_management.Properties.Resources.close__2_;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(673, 442);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(122, 44);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "        Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmFindPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 499);
+            this.Controls.Add(this.btnAddNewPerson);
+            this.Controls.Add(this.btnAddPerson);
+            this.Controls.Add(this.txtIDorNationalNO);
+            this.Controls.Add(this.combFilterBy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.ctrlPersonDetails1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmFindPerson";
+            this.Text = "Find Person";
+            this.Load += new System.EventHandler(this.frmFindPerson_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private ctrlPersonDetails ctrlPersonDetails1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox combFilterBy;
+        private System.Windows.Forms.TextBox txtIDorNationalNO;
+        private System.Windows.Forms.Button btnAddPerson;
+        private System.Windows.Forms.Button btnAddNewPerson;
+    }
+}
---
+++ frmFindPerson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Driving_License_management
+{
+    public partial class frmFindPerson : Form
+    {
+        public frmFindPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddNewPerson_Click(object sender, EventArgs e)
+        {
+            Form frmNewAddPerson = new frmAdd_EditPersonInfo("Add New Person");
+            frmNewAddPerson.ShowDialog();
+        }
+
+        private void frmFindPerson_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
