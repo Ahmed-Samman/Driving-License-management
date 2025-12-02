0a15fde519e09e159d876fa36e676f6d69dcc2d6
Samman <asamman560@gmail.com>
2025-12-02 12:17:01 +0200

    ADD frmPersonDetails UI ( frmPersonDetails.cs + frmPersonDetails.designer.cs + frmPersonDetails.resx )

---
+++ frmPersonDetails.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Driving_License_management
+{
+    partial class frmPersonDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrlPersonDetails1 = new Driving_License_management.ctrlPersonDetails();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(290, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Person Details";
+            // 
+            // ctrlPersonDetails1
+            // 
+            this.ctrlPersonDetails1.Location = new System.Drawing.Point(4, 64);
+            this.ctrlPersonDetails1.Name = "ctrlPersonDetails1";
+            this.ctrlPersonDetails1.Size = new System.Drawing.Size(823, 276);
+            this.ctrlPersonDetails1.TabIndex = 1;
+          
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = global::Driving_License_management.Properties.Resources.close__2_;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(668, 348);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(134, 62);
+            this.btnClose.TabIndex = 27;
+            this.btnClose.Text = "        Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmPersonDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 420);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.ctrlPersonDetails1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPersonDetails";
+            this.Text = "Person Details";
+          
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private ctrlPersonDetails ctrlPersonDetails1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
