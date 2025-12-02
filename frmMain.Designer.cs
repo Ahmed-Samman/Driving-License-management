402c9455d4fba8def1c88ed51f4ae0e83419b311
Samman <asamman560@gmail.com>
2025-12-02 11:05:01 +0200

    Add frmMain UI ( frmMain.cs + frmMain.Designer.cs + frmMain.resx )

---
+++ frmMain.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace UserInterFacelayer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ToolStripMenuItem msDrivingLicenseServices;
+            this.msNewDrivingLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.msLocalLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.msInternationalLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.msRenewDrivingLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.msLostDrivingLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.msDamagedDrivingLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.msReleaseDetaindDrivingLicense = new System.Windows.Forms.ToolStripMenuItem();
+            this.msRetakeTest = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.msApplications = new System.Windows.Forms.ToolStripMenuItem();
+            this.msListApplications = new System.Windows.Forms.ToolStripMenuItem();
+            this.msVehiclesLicenseServices = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.msPeople = new System.Windows.Forms.ToolStripMenuItem();
+            this.msDrivers = new System.Windows.Forms.ToolStripMenuItem();
+            this.msUsers = new System.Windows.Forms.ToolStripMenuItem();
+            this.msAccountSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            msDrivingLicenseServices = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msDrivingLicenseServices
+            // 
+            msDrivingLicenseServices.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msNewDrivingLicense,
+            this.msRenewDrivingLicense,
+            this.toolStripSeparator2,
+            this.msLostDrivingLicense,
+            this.msDamagedDrivingLicense,
+            this.toolStripSeparator3,
+            this.msReleaseDetaindDrivingLicense,
+            this.msRetakeTest});
+            msDrivingLicenseServices.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            msDrivingLicenseServices.Image = global::Driving_License_management.Properties.Resources.id;
+            msDrivingLicenseServices.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            msDrivingLicenseServices.Name = "msDrivingLicenseServices";
+            msDrivingLicenseServices.Size = new System.Drawing.Size(388, 82);
+            msDrivingLicenseServices.Text = "Driving Licenses Services";
+           
+            // 
+            // msNewDrivingLicense
+            // 
+            this.msNewDrivingLicense.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msLocalLicense,
+            this.msInternationalLicense});
+            this.msNewDrivingLicense.Image = global::Driving_License_management.Properties.Resources.user_id;
+            this.msNewDrivingLicense.Name = "msNewDrivingLicense";
+            this.msNewDrivingLicense.Size = new System.Drawing.Size(395, 34);
+            this.msNewDrivingLicense.Text = "New Driving License";
+            // 
+            // msLocalLicense
+            // 
+            this.msLocalLicense.Image = global::Driving_License_management.Properties.Resources.id__4_;
+            this.msLocalLicense.Name = "msLocalLicense";
+            this.msLocalLicense.Size = new System.Drawing.Size(292, 34);
+            this.msLocalLicense.Text = "Local License";
+            // 
+            // msInternationalLicense
+            // 
+            this.msInternationalLicense.Image = global::Driving_License_management.Properties.Resources.id__5_;
+            this.msInternationalLicense.Name = "msInternationalLicense";
+            this.msInternationalLicense.Size = new System.Drawing.Size(292, 34);
+            this.msInternationalLicense.Text = "International License";
+            // 
+            // msRenewDrivingLicense
+            // 
+            this.msRenewDrivingLicense.Image = global::Driving_License_management.Properties.Resources.id__1_;
+            this.msRenewDrivingLicense.Name = "msRenewDrivingLicense";
+            this.msRenewDrivingLicense.Size = new System.Drawing.Size(395, 34);
+            this.msRenewDrivingLicense.Text = "Renew Driving License";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(392, 6);
+            // 
+            // msLostDrivingLicense
+            // 
+            this.msLostDrivingLicense.Image = global::Driving_License_management.Properties.Resources.id__3_;
+            this.msLostDrivingLicense.Name = "msLostDrivingLicense";
+            this.msLostDrivingLicense.Size = new System.Drawing.Size(395, 34);
+            this.msLostDrivingLicense.Text = "Lost Driving License";
+            // 
+            // msDamagedDrivingLicense
+            // 
+            this.msDamagedDrivingLicense.Image = global::Driving_License_management.Properties.Resources.id_config;
+            this.msDamagedDrivingLicense.Name = "msDamagedDrivingLicense";
+            this.msDamagedDrivingLicense.Size = new System.Drawing.Size(395, 34);
+            this.msDamagedDrivingLicense.Text = "Damaged Driving License";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(392, 6);
+            // 
+            // msReleaseDetaindDrivingLicense
+            // 
+            this.msReleaseDetaindDrivingLicense.Image = global::Driving_License_management.Properties.Resources.id_delete;
+            this.msReleaseDetaindDrivingLicense.Name = "msReleaseDetaindDrivingLicense";
+            this.msReleaseDetaindDrivingLicense.Size = new System.Drawing.Size(395, 34);
+            this.msReleaseDetaindDrivingLicense.Text = "Release Detained Driving License";
+            // 
+            // msRetakeTest
+            // 
+            this.msRetakeTest.Image = global::Driving_License_management.Properties.Resources.test_edit;
+            this.msRetakeTest.Name = "msRetakeTest";
+            this.msRetakeTest.Size = new System.Drawing.Size(395, 34);
+            this.msRetakeTest.Text = "Retake Test";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msApplications,
+            this.msPeople,
+            this.msDrivers,
+            this.msUsers,
+            this.msAccountSetting});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(1304, 103);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+           
+            // msApplications
+            // 
+            this.msApplications.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msListApplications,
+            msDrivingLicenseServices,
+            this.msVehiclesLicenseServices,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem5});
+            this.msApplications.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msApplications.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.msApplications.Image = global::Driving_License_management.Properties.Resources.public_document2;
+            this.msApplications.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msApplications.Name = "msApplications";
+            this.msApplications.Size = new System.Drawing.Size(106, 99);
+            this.msApplications.Text = " ";
+            this.msApplications.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.msApplications.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            // 
+            // msListApplications
+            // 
+            this.msListApplications.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msListApplications.Image = global::Driving_License_management.Properties.Resources.list;
+            this.msListApplications.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msListApplications.Name = "msListApplications";
+            this.msListApplications.Size = new System.Drawing.Size(388, 82);
+            this.msListApplications.Text = "List Applications";
+            // 
+            // msVehiclesLicenseServices
+            // 
+            this.msVehiclesLicenseServices.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msVehiclesLicenseServices.Image = global::Driving_License_management.Properties.Resources.cars;
+            this.msVehiclesLicenseServices.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msVehiclesLicenseServices.Name = "msVehiclesLicenseServices";
+            this.msVehiclesLicenseServices.Size = new System.Drawing.Size(388, 82);
+            this.msVehiclesLicenseServices.Text = "Vehicles Licenses services";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(385, 6);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem5.Image = global::Driving_License_management.Properties.Resources.company;
+            this.toolStripMenuItem5.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(388, 82);
+            this.toolStripMenuItem5.Text = "Manage Application Types";
+           
+            // 
+            // msPeople
+            // 
+            this.msPeople.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msPeople.ForeColor = System.Drawing.Color.Teal;
+            this.msPeople.Image = global::Driving_License_management.Properties.Resources.our_team;
+            this.msPeople.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msPeople.Name = "msPeople";
+            this.msPeople.Size = new System.Drawing.Size(163, 99);
+            this.msPeople.Text = "People";
+            this.msPeople.Click += new System.EventHandler(this.msPeople_Click);
+            // 
+            // msDrivers
+            // 
+            this.msDrivers.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msDrivers.Image = global::Driving_License_management.Properties.Resources.our_team2;
+            this.msDrivers.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msDrivers.Name = "msDrivers";
+            this.msDrivers.Size = new System.Drawing.Size(168, 99);
+            this.msDrivers.Text = "Drivers";
+            // 
+            // msUsers
+            // 
+            this.msUsers.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msUsers.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.msUsers.Image = global::Driving_License_management.Properties.Resources.users_write;
+            this.msUsers.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msUsers.Name = "msUsers";
+            this.msUsers.Size = new System.Drawing.Size(151, 99);
+            this.msUsers.Text = "Users";
+            this.msUsers.Click += new System.EventHandler(this.msUsers_Click);
+            // 
+            // msAccountSetting
+            // 
+            this.msAccountSetting.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msAccountSetting.ForeColor = System.Drawing.Color.Olive;
+            this.msAccountSetting.Image = global::Driving_License_management.Properties.Resources.account_settings;
+            this.msAccountSetting.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.msAccountSetting.Name = "msAccountSetting";
+            this.msAccountSetting.Size = new System.Drawing.Size(252, 99);
+            this.msAccountSetting.Text = "Account Setting";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(597, 546);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Logged User";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Image = global::Driving_License_management.Properties.Resources.bald_eagle_wings_open_vector1;
+            this.pictureBox1.Location = new System.Drawing.Point(500, 261);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 282);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 593);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "frmMain";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem msApplications;
+        private System.Windows.Forms.ToolStripMenuItem msPeople;
+        private System.Windows.Forms.ToolStripMenuItem msDrivers;
+        private System.Windows.Forms.ToolStripMenuItem msUsers;
+        private System.Windows.Forms.ToolStripMenuItem msAccountSetting;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem msListApplications;
+        private System.Windows.Forms.ToolStripMenuItem msVehiclesLicenseServices;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem msNewDrivingLicense;
+        private System.Windows.Forms.ToolStripMenuItem msRenewDrivingLicense;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem msLostDrivingLicense;
+        private System.Windows.Forms.ToolStripMenuItem msDamagedDrivingLicense;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem msReleaseDetaindDrivingLicense;
+        private System.Windows.Forms.ToolStripMenuItem msRetakeTest;
+        private System.Windows.Forms.ToolStripMenuItem msLocalLicense;
+        private System.Windows.Forms.ToolStripMenuItem msInternationalLicense;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,38 @@
+﻿using Driving_License_management;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterFacelayer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void msPeople_Click(object sender, EventArgs e)
+        {
+            Form frmMPeople = new frmManagePeople();
+            frmMPeople.ShowDialog();
+        }
+
+        private void msUsers_Click(object sender, EventArgs e)
+        {
+            Form frmMUsers = new frmMangeUser();
+            frmMUsers.ShowDialog();
+        }
+    }
+}
