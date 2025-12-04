897648662ef6836fd5e4b7e0e0e963ac69ff22a2
Samman <asamman560@gmail.com>
2025-12-04 09:46:05 +0200

    Add Form (frmChangePassword.cs + frmChangePassword.designer.cs + frmChangePassword.resx)

---
+++ frmChangePassword.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Driving_License_management
+{
+    partial class frmChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcurrentPassword = new System.Windows.Forms.TextBox();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmpassword = new System.Windows.Forms.TextBox();
+            this.ctrlPersonDetailsAndlogin1 = new Driving_License_management.ctrlPersonDetailsAndlogin();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Image = global::Driving_License_management.Properties.Resources.diskette__2_;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(729, 605);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(142, 58);
+            this.btnSave.TabIndex = 30;
+            this.btnSave.Text = "        Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = global::Driving_License_management.Properties.Resources.close__2_;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(565, 605);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(140, 58);
+            this.btnClose.TabIndex = 29;
+            this.btnClose.Text = "        Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = global::Driving_License_management.Properties.Resources.password;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(36, 472);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Current Password:         ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = global::Driving_License_management.Properties.Resources.password;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(62, 529);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 20);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "New Password:         ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Image = global::Driving_License_management.Properties.Resources.password;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(36, 581);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 20);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Confirm Password:         ";
+            // 
+            // txtcurrentPassword
+            // 
+            this.txtcurrentPassword.Location = new System.Drawing.Point(268, 469);
+            this.txtcurrentPassword.Name = "txtcurrentPassword";
+            this.txtcurrentPassword.Size = new System.Drawing.Size(203, 26);
+            this.txtcurrentPassword.TabIndex = 34;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Location = new System.Drawing.Point(268, 523);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.Size = new System.Drawing.Size(203, 26);
+            this.txtNewPassword.TabIndex = 35;
+            // 
+            // txtConfirmpassword
+            // 
+            this.txtConfirmpassword.Location = new System.Drawing.Point(268, 575);
+            this.txtConfirmpassword.Name = "txtConfirmpassword";
+            this.txtConfirmpassword.Size = new System.Drawing.Size(203, 26);
+            this.txtConfirmpassword.TabIndex = 36;
+            // 
+            // ctrlPersonDetailsAndlogin1
+            // 
+            this.ctrlPersonDetailsAndlogin1.Location = new System.Drawing.Point(10, 2);
+            this.ctrlPersonDetailsAndlogin1.Name = "ctrlPersonDetailsAndlogin1";
+            this.ctrlPersonDetailsAndlogin1.Size = new System.Drawing.Size(861, 448);
+            this.ctrlPersonDetailsAndlogin1.TabIndex = 0;
+            // 
+            // frmChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 687);
+            this.Controls.Add(this.txtConfirmpassword);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.txtcurrentPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.ctrlPersonDetailsAndlogin1);
+            this.Name = "frmChangePassword";
+            this.Text = "Change Password";
+            this.Load += new System.EventHandler(this.frmChangePassword_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ctrlPersonDetailsAndlogin ctrlPersonDetailsAndlogin1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcurrentPassword;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.TextBox txtConfirmpassword;
+    }
+}
