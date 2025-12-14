e3c0abcd3e6b95fd14d121020f30c4ef8fa55df3
Samman <asamman560@gmail.com>
2025-12-14 18:13:27 +0200

    ADD New Method (_AddNewPerson) To Call PeopleAccessLayer To Add A New Person

--- DVLDBusinessLayer/clsPeopleBusinessLayer.cs
+++ DVLDBusinessLayer/clsPeopleBusinessLayer.cs
@@ -9,21 +9,25 @@ namespace DVLDBusinessLayer
     {
         public int PersonID { get; set; }
         public int NationalNO { get; set; }
+        public int NationalityCountryID { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string ThirdName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string Gender { get; set; }
+        public byte Gender { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public string ImagePath { get; set; }
 
 
-        clsPeopleBusinessLayer(int PersonID, int NationalNO, string FirstName, string SecondName,
-            string ThirdName, string LastName, DateTime DateOfBirth, string Gender, string Address, string Phone, string Email)
+        clsPeopleBusinessLayer(int PersonID, int NationalNO, int CountryID, string FirstName, string SecondName,
+            string ThirdName, string LastName, DateTime DateOfBirth, byte Gender, string Address, string Phone, string Email)
         {
             this.PersonID = PersonID;
+            this.NationalNO = NationalNO;
+            this.NationalityCountryID = CountryID;
             this.FirstName = FirstName;
             this.SecondName = SecondName;
             this.ThirdName = ThirdName;
@@ -35,10 +39,20 @@ namespace DVLDBusinessLayer
             this.Email = Email;
         }
 
+
         static public DataTable List()
         {
             return clsPeopleDataAccess.GetAllTable();
         }
+
+        private bool _AddNewPerson()
+        {
+            this.PersonID = clsPeopleDataAccess.AddNewPerson(NationalNO, FirstName, SecondName, 
+                ThirdName, LastName, DateOfBirth, Gender, Address, Phone, Email, NationalityCountryID, ImagePath);
+            return (this.PersonID == -1);
+        }
+
+
 
     }
 }
