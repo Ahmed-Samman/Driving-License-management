a10daecf92c1b849f81741cdb296c3c39e8354ba
Samman <asamman560@gmail.com>
2025-12-19 19:02:44 +0200

    ADD New Some Methodes Search_NationalNO() To Search about National Number Is Existed In DataBase Through clsDataAccessLayer , GetAllCountries() To Get All Country From Database Through clsCountryDataAccessLayer, Save() To Call it From (UserCountrol) ctrlAddEditPerson By Object And Save Data In Database

--- DVLDBusinessLayer/clsPeopleBusinessLayer.cs
+++ DVLDBusinessLayer/clsPeopleBusinessLayer.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
 using DVLDDataAccessLayer;
 
 namespace DVLDBusinessLayer
 {
     public class clsPeopleBusinessLayer
     {
+        public enum enMode { AddMode = 0, UpdateMode = 1 };
+        enMode Mode = enMode.UpdateMode;
+
+
         public int PersonID { get; set; }
-        public int NationalNO { get; set; }
+        public string NationalNO { get; set; }
         public int NationalityCountryID { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
@@ -21,8 +27,24 @@ namespace DVLDBusinessLayer
         public string Email { get; set; }
         public string ImagePath { get; set; }
 
+        public clsPeopleBusinessLayer()
+        {
+            this.PersonID = -1;
+            this.NationalNO = "";
+            this.NationalityCountryID = -1;
+            this.FirstName = "";
+            this.SecondName = "";
+            this.ThirdName = "";
+            this.LastName = "";
+            this.DateOfBirth = DateTime.Now;
+            this.Gender = 0;
+            this.Phone = "";
+            this.Email = "";
+            this.ImagePath = "";
+            Mode = enMode.AddMode;
+        }
 
-        clsPeopleBusinessLayer(int PersonID, int NationalNO, int CountryID, string FirstName, string SecondName,
+        clsPeopleBusinessLayer(int PersonID, string NationalNO, int CountryID, string FirstName, string SecondName,
             string ThirdName, string LastName, DateTime DateOfBirth, byte Gender, string Address, string Phone, string Email)
         {
             this.PersonID = PersonID;
@@ -49,10 +71,39 @@ namespace DVLDBusinessLayer
         {
             this.PersonID = clsPeopleDataAccess.AddNewPerson(NationalNO, FirstName, SecondName, 
                 ThirdName, LastName, DateOfBirth, Gender, Address, Phone, Email, NationalityCountryID, ImagePath);
-            return (this.PersonID == -1);
+            return (this.PersonID != -1);
         }
 
+        static public bool Search_NationalNO(string National_NO)
+        {
+             return clsPeopleDataAccess.IsNationalNO_Exist(National_NO);
+        }
 
+        static public DataTable GetAllCountries()
+        {
+            return clsCountryDataAccess.GetAllCountries_FromDatabase();
+        }
+       
+        public bool Save()
+        {
+            switch(Mode)
+            {
+                case enMode.AddMode:
+                    if (_AddNewPerson())
+                    {
+                        Mode = enMode.UpdateMode;
+                        return true;
+                    }
+                    break;
+                    
+                //case enMode.UpdateMode:
+                //    {
 
+                //    }           
+            }
+
+            return false;
+
+        }
     }
 }
