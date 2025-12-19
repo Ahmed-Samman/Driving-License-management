d324e90bf38a3786c14ba8537ce3735f8b8e82d5
Samman <asamman560@gmail.com>
2025-12-19 19:16:13 +0200

    ADD clsCountryDataAccessLayer To Get All Countries From database And Put Them In Combobox

---
+++ DVLDDataAccessLayer/clsCountryDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDDataAccessLayer
+{
+    public class clsCountryDataAccess
+    {
+
+        static public DataTable GetAllCountries_FromDatabase()
+        {
+            DataTable dt = new DataTable();
+            string query = @"SELECT * FROM Countries;";
+
+            SqlConnection connection = new SqlConnection(clsConectionWithDataBase.ConectionWithDataBase);
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if(reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                
+                ex.Message.ToString();
+                dt = null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dt;
+        }
+
+    }
+}
