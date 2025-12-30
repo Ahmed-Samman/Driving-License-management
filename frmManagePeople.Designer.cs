3ea44094321c170b872b127d186e5a582582eaaf
Samman <asamman560@gmail.com>
2025-12-30 16:59:32 +0200

    ADD ShowPerson , AddNewPerson, UpdatePerson, DeletePerson Methods To open Show Forms To The user

--- frmManagePeople.Designer.cs
+++ frmManagePeople.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvManagePeople = new System.Windows.Forms.DataGridView();
             this.contmList = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tsmShowDetails = new System.Windows.Forms.ToolStripMenuItem();
@@ -62,14 +62,14 @@
             this.dgvManagePeople.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvManagePeople.ColumnHeadersHeight = 34;
             this.dgvManagePeople.ContextMenuStrip = this.contmList;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvManagePeople.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvManagePeople.DefaultCellStyle = dataGridViewCellStyle1;
             this.dgvManagePeople.GridColor = System.Drawing.SystemColors.AppWorkspace;
             this.dgvManagePeople.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.dgvManagePeople.Location = new System.Drawing.Point(1, 269);
@@ -79,7 +79,7 @@
             this.dgvManagePeople.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.dgvManagePeople.Size = new System.Drawing.Size(1487, 276);
             this.dgvManagePeople.TabIndex = 0;
-            this.dgvManagePeople.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvManagePeople_CellContentClick);
+            this.dgvManagePeople.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvManagePeople_CellMouseDown);
             // 
             // contmList
             // 
@@ -132,6 +132,7 @@
             this.tsmDelete.Name = "tsmDelete";
             this.tsmDelete.Size = new System.Drawing.Size(224, 32);
             this.tsmDelete.Text = "Delete";
+            this.tsmDelete.Click += new System.EventHandler(this.tsmDelete_Click);
             // 
             // toolStripSeparator2
             // 
--- frmManagePeople.cs
+++ frmManagePeople.cs
@@ -25,47 +25,22 @@ namespace UserInterFacelayer
 
         }
 
+        private void _RefreshDataGrid()
+        {
+            dt = clsPeopleBusinessLayer.List();
+            dgvManagePeople.DataSource = dt;
+        }
 
+        // Add New Person (Button)
         private void btnAddPerson_Click(object sender, EventArgs e)
         {
             Form frmAddEditPerson = new frmAdd_EditPersonInfo("Add New Person");
             frmAddEditPerson.ShowDialog();
             _RefreshDataGrid();
         }
-        private void tsmShowDetails_Click(object sender, EventArgs e)
-        {
-            Form frmDetailP = new frmPersonDetails();
-            frmDetailP.ShowDialog();
-        }
-        private void tsmAddNewPerson_Click(object sender, EventArgs e)
-        {
-            Form frmAddEditPerson = new frmAdd_EditPersonInfo("Add New Person");
-            frmAddEditPerson.ShowDialog();
-            _RefreshDataGrid();
-        }
-        private void tsmEdit_Click(object sender, EventArgs e)
-        {
-            Form frmAddEditPerson = new frmAdd_EditPersonInfo("     Update Person");      
-            frmAddEditPerson.ShowDialog();
-        }
-
-      
-
-        private void _RefreshDataGrid()
-        {
-            dt = clsPeopleBusinessLayer.List();
-            dgvManagePeople.DataSource = dt;
-            
-            // For Hide A Column
-            dgvManagePeople.Columns["NationalityCountryID"].Visible = false;
-            dgvManagePeople.Columns["ImagePath"].Visible = false;
-        }
-
-        private void dgvManagePeople_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }
-
+        
+        
+        // Search About Person By Filter
         private void _AddFilterBy()
         {
             combFilterBy.Items.Add("None");
@@ -84,19 +59,22 @@ namespace UserInterFacelayer
         }
 
 
+        // Choice Witch Column To Search By it
         private void combFilterBy_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(combFilterBy.SelectedIndex != 0)
-            {     
-                txtbSearchBy.Visible = true;               
+            if (combFilterBy.SelectedIndex != 0)
+            {
+                txtbSearchBy.Visible = true;
             }
             else
-            {    
+            {
                 txtbSearchBy.Visible = false;
                 dgvManagePeople.DataSource = dt;
             }
         }
 
+
+        // To Search By People Column Of Info
         private void txtbSearchBy_TextChanged(object sender, EventArgs e)
         {
 
@@ -112,7 +90,73 @@ namespace UserInterFacelayer
             {
                 dv.RowFilter = $"{combFilterBy.SelectedItem} like '{txtbSearchBy.Text}%'";
             }
-            dgvManagePeople.DataSource = dv;  
+            dgvManagePeople.DataSource = dv;
         }
+
+
+        // ---------------------- ContextMenuStrip ------------------//
+
+        private void dgvManagePeople_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && e.RowIndex >= 0)
+            {
+                dgvManagePeople.ClearSelection();
+                dgvManagePeople.Rows[e.RowIndex].Selected = true;
+                dgvManagePeople.CurrentCell = dgvManagePeople.Rows[e.RowIndex].Cells[0];
+            }
+        }
+
+
+        //1- Show Person Info
+        private void tsmShowDetails_Click(object sender, EventArgs e)
+        {
+            Form frmDetailPerson = new frmPersonDetails();
+            frmDetailPerson.ShowDialog();
+        }
+
+
+        //2- Add new Person
+        private void tsmAddNewPerson_Click(object sender, EventArgs e)
+        {
+            Form frmAddEditPerson = new frmAdd_EditPersonInfo("Add New Person");
+            frmAddEditPerson.ShowDialog();
+            _RefreshDataGrid();
+        }
+
+
+        //3- Update Person Info
+        private void tsmEdit_Click(object sender, EventArgs e)
+        {
+            Form frmAddEditPerson = new frmAdd_EditPersonInfo("     Update Person");      
+            frmAddEditPerson.ShowDialog();
+        }
+
+
+        //4- Delete Person 
+        private void tsmDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvManagePeople.SelectedRows.Count == 0)
+                return;
+
+            int PersonID = Convert.ToInt32(dgvManagePeople.SelectedRows[0].Cells["PersonID"].Value);
+
+            if (MessageBox.Show("Do You Want To Delete This Person?", "Delete", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                bool CanDelete = clsPeopleBusinessLayer.IsDeleted(PersonID);
+
+                if (CanDelete)
+                {
+                    MessageBox.Show("You Deleted Succesfuly", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    _RefreshDataGrid();
+                }
+                else
+                {
+                    MessageBox.Show("You Cannot Delete This Person!", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }          
+        }
+
+       
+      
     }
 }
