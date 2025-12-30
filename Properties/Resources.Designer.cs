a90e163c2de0017f7ea7b95d5e99de8bdd9bc692
Samman <asamman560@gmail.com>
2025-12-30 17:01:52 +0200

    ADD Some Of Pictuers To Resources.Designer

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -533,6 +533,16 @@ namespace Driving_License_management.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap sign_out {
+            get {
+                object obj = ResourceManager.GetObject("sign_out", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap test_edit {
             get {
                 object obj = ResourceManager.GetObject("test_edit", resourceCulture);
